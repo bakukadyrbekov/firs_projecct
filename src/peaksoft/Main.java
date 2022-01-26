d33362692a68d3c9ea60a52996baa049f1009b73
Barpybek Kadyrbekov <barpybek2002@gmail.com>
2022-01-26 21:21:07 +0600

    hello

---
+++ src/peaksoft/Main.java
@@ -0,0 +1,32 @@
+package peaksoft;
+
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+//        System.out.println(someMethod(2,2,4));
+//   }
+//       static int someMethod(int ... a) {
+//        int sum=0;
+//        for (int i = 0; i < a.length; i++) {
+//           sum+=a[i];
+//
+//        } return sum;
+        String name = "    Hello World!   ";
+
+        String mewName = name.toLowerCase();
+
+
+       String newWorld;
+        System.out.println("Hello");
+     }
+    }
+
+
+
+
+
+
---
+++ src/peaksoft/Main2.java
@@ -0,0 +1,23 @@
+package peaksoft;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main2 {
+    public static void main(String[] args) {
+        Random random = new Random();
+
+        double randomNamber1 = random.nextInt(15);
+       // double randomNamber2 = random.nextInt();
+       // double randomNamber3 = random.nextInt();
+
+        System.out.println(randomNamber1);
+
+
+
+
+
+
+
+    }
+}
